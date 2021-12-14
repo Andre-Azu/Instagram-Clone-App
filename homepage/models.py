cdc5c5efc1eb8a7260cf307b37c4218d45a94325
andre azu <andreazuaa@gmail.com>
2021-12-14 08:02:35 +0300

    add image model

--- homepage/models.py
+++ homepage/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image = models.ImageField(upload_to='instagram',default=None)
+    image_name = models.CharField(max_length=30)
+    image_caption = models.CharField(max_length=1000)
+    
