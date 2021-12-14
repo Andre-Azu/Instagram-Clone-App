dbd0303d04c10cd85ccbcf3f3c3f998ee3906845
andre azu <andreazuaa@gmail.com>
2021-12-14 08:52:38 +0300

    add changes

--- homepage/urls.py
+++ homepage/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url
 
+
 urlpatterns = [
     url('^$', views.homepage_images,name='home'),
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Image
 # Create your views here.
 def homepage_images(request):
-    images=Image.objects.all()
-    return render(request, 'homepage.html', {'images':images })
+    # images=Image.objects.all()
+    return render(request, 'homepage.html')
