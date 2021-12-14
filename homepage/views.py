bc29e448a82ba53ebe09a5b22a44fcd399d8c70e
andre azu <andreazuaa@gmail.com>
2021-12-14 08:31:47 +0300

    add urls.py file

--- homepage/views.py
+++ homepage/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Image
 # Create your views here.
-def all_images(request):
+def homepage_images(request):
     images=Image.objects.all()
-    return render(request, 'home.html', {'images':images })
+    return render(request, 'homepage.html', {'images':images })
