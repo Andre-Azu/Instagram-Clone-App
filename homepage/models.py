de2428218182c463a1bf205b12214653cc53058f
andre azu <andreazuaa@gmail.com>
2021-12-14 08:24:58 +0300

    add image methods

--- homepage/models.py
+++ homepage/models.py
@@ -6,3 +6,8 @@ class Image(models.Model):
     image_name = models.CharField(max_length=30)
     image_caption = models.CharField(max_length=1000)
     
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
