91e5b6c8b21348325f912a17f1cf5d0b7a1975ee
andre azu <andreazuaa@gmail.com>
2021-12-14 08:23:39 +0300

    add first view

--- homepage/views.py
+++ homepage/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Image
 # Create your views here.
+def all_images(request):
+    images=Image.objects.all()
+    return render(request, 'home.html', {'images':images })
