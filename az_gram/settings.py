c152950669b6420a418cba503a78f52a05d1cbfa
andre azu <andreazuaa@gmail.com>
2021-12-14 09:16:11 +0300

    change to bootstrap 3

--- az_gram/settings.py
+++ az_gram/settings.py
@@ -32,13 +32,14 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'homepage',
-
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 'bootsrtap3',
 ]
 
 MIDDLEWARE = [
