4f0a09188215a4d00fe7e3726a7ae3da6ae21b63
andre azu <andreazuaa@gmail.com>
2021-12-14 09:52:51 +0300

    add user profile html

--- az_gram/urls.py
+++ az_gram/urls.py
@@ -18,5 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path(r'^admin/', admin.site.urls),
-    path(r'',include('homepage.urls'))
+    path(r'',include('homepage.urls')),
+    path(r'',include('userprofile.urls'))
 ]
---
+++ userprofile/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+# from django.conf.urls import url
+
+
+urlpatterns = [
+    path('profile', views.my_profile,name='profile'),
+]
