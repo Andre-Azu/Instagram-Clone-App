2959bff9001670ad20da662b55a99521746ac775
andre azu <andreazuaa@gmail.com>
2021-12-14 09:43:46 +0300

    Register the models to the admin page

--- userprofile/admin.py
+++ userprofile/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from django.db.models.fields import FilePathField
+from .models import Userprofile,Follows
 
 # Register your models here.
+admin.site.register(Userprofile)
+admin.site.register(Follows)
