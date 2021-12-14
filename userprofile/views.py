8e9ba680c522876c9fb246e23c9839fd3cae6fe4
andre azu <andreazuaa@gmail.com>
2021-12-14 09:45:42 +0300

    add homepage view

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Userprofile, Follows
 # Create your views here.
+def my_profile(request):
+    
+    return render(request, 'profile.html')
